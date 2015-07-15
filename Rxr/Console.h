1ce9ff92dfb387c5faaa78fd83a503235a5eb087
thayerjbirch <balcoth@gmail.com>
2015-07-15 10:31:36 -0700

    Console Version Numbers
    as titled

--- Rxr/Console.h
+++ Rxr/Console.h
@@ -5,7 +5,7 @@
 
 // this shouldn't go here, but it's late and I'm tired
 // and Console IS the only place it's referenced
-#define CURRENT_VERSION "1.0.0"
+#define CURRENT_VERSION "1.0.3"
 
 class String;
 
